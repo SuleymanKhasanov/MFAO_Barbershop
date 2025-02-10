a80f083ebfffdba426bdbea47f152ff62459dd0a
Сулейман Хасанов <xasanovsuleyman@gmail.com>
2025-02-10 15:11:48 +0500

    Added frontend configuration

--- frontend/.next/server/server-reference-manifest.js
+++ frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"XOhokzb0l7+CHTz1HZkDuU8Ziqu5RFQRXxOZEI8oPmg=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
